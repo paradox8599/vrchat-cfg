5a3f88345d7fe64dd94310f7d8f03ae667b67a15
paradox8599 <paradox8599@outlook.com>
2023-08-17 00:44:37 +1000

    Removed unused anyhow crate

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use anyhow::Result;
 use err::AppError;
 
 pub mod cfg;
